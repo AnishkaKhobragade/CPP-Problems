30c24b55edd81944d48168425e50e60acb1b24cd
Anishka Khobragade <132399566+AnishkaKhobragade@users.noreply.github.com>
2025-05-14 14:28:00 -0700

    Add files via upload

---
+++ mixed_up1.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+using namespace std; 
+
+int main() 
+{ 
+    // A crazy mixed up program 
+    cout << "In 1492 Columbus sailed the ocean blue."; 
+
+    return 0 ; 
+} 
---
+++ mixed_up2.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+using namespace std; 
+
+int main()
+{
+    //It's a mad, mad program 
+    cout << "Success\n";
+    cout << "Success "; 
+    cout << "Success\n"; 
+    cout << "Success\n\n";
+    return 0;
+}
